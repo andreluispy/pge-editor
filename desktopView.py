6877f8696127c01e21014597a8508c6e063831c2
AndréL <andreluisportelaneves@gmail.com>
2021-12-21 16:16:35 -0300

    Modal size FIX and Desktop Version Removed

--- desktopView.py
+++
@@ -1,9 +0,0 @@
-import webview
-import tkinter as tk
-
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-webview.create_window('PGE - Engine', './index.html', width=screen_width, height=screen_height)
-webview.start()
