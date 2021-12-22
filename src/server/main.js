991d1f93a73286033b88ac77f6f630105a67b5f4
DutraGames <gabrieldutraamarante@gmail.com>
2021-12-22 19:38:33 -0300

    Process close add in main

--- src/server/main.js
+++ src/server/main.js
@@ -1,17 +1,24 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const ipc = ipcMain
 
 function createWindow () {
     const win = new BrowserWindow({
       width: 1080,
       height: 720,
       webPreferences:{
-        nodeIntegration: true
+        nodeIntegration: true,
+        contextIsolation: false,
       },
     })
 
     win.loadFile('index.html')
 
     win.setMenuBarVisibility(false)
+
+    // CLOSE APP
+    ipc.on('closeProject', () =>{
+      win.close()
+    })
 }
 
 app.whenReady().then(() => {
