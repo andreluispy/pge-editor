f4d799150e62ca1f0022cb351404683996687629
DutraGames <gabrieldutraamarante@gmail.com>
2021-12-22 13:53:42 -0300

    permission to NodeIntegrations

--- src/server/main.js
+++ src/server/main.js
@@ -4,9 +4,13 @@ function createWindow () {
     const win = new BrowserWindow({
       width: 1080,
       height: 720,
+      webPreferences:{
+        nodeIntegration: true
+      },
     })
 
     win.loadFile('index.html')
+
     win.setMenuBarVisibility(false)
 }
 
