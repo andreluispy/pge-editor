912d466149deccace7a38eb0cb2a5cd37d9d0841
DutraGames <gabrieldutraamarante@gmail.com>
2021-12-21 21:39:25 -0300

    create name generator

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+// Vars
+var objs_html = document.getElementById("objs")
+var objs = []
+var props_html = document.getElementsByClassName("props")[0]
+
+var mousePos = {x: 0, y: 0}
+var toReturn = null
+
+var modal = document.getElementById("ModalBox")
+var span = document.getElementsByClassName("close")[0]
+var confirm = document.getElementById("confirmAdd")
+
+span.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
+
+function modalConfirm() {
+    modal.style.display = "none"
+
+    let objType = document.getElementById("objToAdd").value
+    
+    if (objType == "rect"){
+        toReturn = {
+            name: 'NewRect' + countObjs('rect'),
+            rendering: new Rect(mousePos.x.toFixed(2), mousePos.y.toFixed(2), 32, 32, "#0000ff")
+        }
+    }
+    else if (objType == "circle"){
+        toReturn = {
+            name: 'NewCircle' + countObjs('circle'),
+            rendering: new Circle(mousePos.x.toFixed(2), mousePos.y.toFixed(2), 32, "#0000ff")
+        }
+    }
+    else if (objType == "text"){
+        toReturn = {
+            name: 'NewText' + countObjs('text'),
+            rendering: new CText("Text", mousePos.x.toFixed(2), mousePos.y.toFixed(2), "#ffffff", "25px Arial")
+        }
+    }
+
+    objs.push(toReturn)
+    prop_Load(objs.length-1)
+}
+
+confirm.onclick = modalConfirm
+confirm.onchange = modalConfirm
+
+function update_editor() {
+    // Update Objects View
+    var to_write = makeObjHtml()
+    if (objs_html.innerHTML !== to_write) {
+        objs_html.innerHTML = to_write
+    }
+
+    // Add Objects
+    canvas.onclick = event => {
+        if (event.detail === 2) {
+            mousePos = getMousePos(event)
+            toReturn = null
+            modal.style.display = "block"
+        }
+    }
+}
+
+update(update_editor)
+
+function countObjs(objType){
+    let i = 0
+    for (obj in objs){
+        if (objs[obj].rendering.type == objType){
+            i++;
+        }
+    }
+    return i
+}
