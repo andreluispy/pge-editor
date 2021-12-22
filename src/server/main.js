ad1c397d460c596e9e1e712556020c3a69c78883
DutraGames <gabrieldutraamarante@gmail.com>
2021-12-21 21:38:56 -0300

    menu unvisible

---
+++ src/server/main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 1080,
+      height: 720,
+    })
+
+    win.loadFile('index.html')
+    win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
