9363a404c44e4bad51699a86547689b5f2bdbe1d
DutraGames <gabrieldutraamarante@gmail.com>
2021-12-22 21:12:14 -0300

    OpenIDE, maxScreen

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,8 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const ipc = ipcMain
 
-function createWindow () {
+
+const createWindow = () =>{
     const win = new BrowserWindow({
       width: 1080,
       height: 720,
@@ -12,15 +13,38 @@ function createWindow () {
     })
 
     win.loadFile('index.html')
+    
+    // Leave the screen at its maximum size
+    win.maximize()
 
+    //Menu Bar false
     win.setMenuBarVisibility(false)
 
     // CLOSE APP
     ipc.on('closeProject', () =>{
       win.close()
     })
+
+    //  OPEN IDE
+    ipc.on('openIDE', ChildWin)
 }
+
 
 app.whenReady().then(() => {
     createWindow()
 })
+
+// Function Opem IDE
+const ChildWin = ()=>{
+  const win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences:{
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  win.loadFile('IDE.html')
+  win.setMenuBarVisibility(false)
+}
