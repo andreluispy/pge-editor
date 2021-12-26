822ae84dfa15d24ef8f7e3a4943de2ecd5ee52e4
AndréL <andreluisportelaneves@gmail.com>
2021-12-25 21:58:30 -0300

    IDE font control

--- src/js/ideFunctions.js
+++ src/js/ideFunctions.js
@@ -5,17 +5,27 @@ const fs = require("fs")
 let keys = {}
 setInterval(function () {document.addEventListener('keydown', function (e) {keys[e.key] = true}); document.addEventListener('keyup', function (e) {delete keys[e.key]})}, 25)
 function get_pressed(_key) {if (_key in keys) {return true == true}}
+let editor_font_size = 25
 
 fs.readFile('projects/code.py', 'utf8', function(err, data) {
     document.getElementById("editor").innerHTML = data
     var editor = ace.edit("editor")
     editor.setTheme("ace/theme/monokai")
     editor.session.setMode("ace/mode/python")
+    editor.setFontSize(editor_font_size)
 
     // EXPORT FILE
     setInterval(function () {
         if (get_pressed('Control') && get_pressed('s')) {
             ipc.send('saveScript', editor.getValue())
+        }
+        if (get_pressed('Control') && get_pressed('.')) {
+            editor_font_size++
+            editor.setFontSize(editor_font_size)
+        }
+        if (get_pressed('Control') && get_pressed(',')) {
+            editor_font_size--
+            editor.setFontSize(editor_font_size)
         }
     }, 25)
 })
